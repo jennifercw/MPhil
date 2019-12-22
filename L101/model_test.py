dd9aeb8e093d368adce6ff12b4ff14bc0841e24c
jennifercw <jcrw122@googlemail.com>
2019-12-22 22:42:20 +0000

    Added reverse model ranking and coverage penalty

--- L101/model_test.py
+++ L101/model_test.py
@@ -3,6 +3,7 @@ from tensorflow.python.keras.models import Model, load_model
 from tensorflow.python.keras.layers import Attention
 import numpy as np
 import pandas as pd
+from scipy.special import softmax
 import math
 import random
 import pickle
@@ -115,6 +116,37 @@ def load_data_chars(filename="trainset.csv", delex_slots=(), delex_both=True):
     return data_details
 
 
+def load_data_chars_reverse(filename="trainset.csv", delex_slots=(), delex_both=True):
+    """
+    Output is dictionary containing:
+    "mr_input" : List of all MRs, "ref_list": List of all reference sentences
+    "char_set" : List of all characters in MRs and references
+    "num_chars" : number of characters that appear
+    "max_mr_length" : max length of MR seqs, "max_ref_length: max length of reference seqs
+    """
+    start = '\t'
+    end = '\n'
+    file = pd.read_csv(filename)
+    mr_list = file['mr'].to_list()
+    ref_list = file['ref'].to_list()
+    mr_list = [start + mr + end for mr in mr_list]
+    if len(delex_slots) > 0:
+        mr_list, ref_list = delexicalise_both(mr_list, ref_list, delex_slots)
+    else:
+        mr_list = delexicalise_mrs(mr_list, delex_slots)
+    assert(len(mr_list) == len(ref_list))
+    char_set = pickle.load(open("char_set.obj", "rb"))
+    char_set = sorted(list(char_set))
+    num_chars = len(char_set)
+    max_mr_length = max([len(mr) for mr in mr_list])
+    max_ref_length = max([len(ref) for ref in ref_list])
+
+    data_details = {'mr_input': mr_list, 'ref_sentences': ref_list, 'char_set': char_set, 'num_chars': num_chars,
+                    'max_mr_length': max_mr_length, 'max_ref_length': max_ref_length}
+    return data_details
+
+
+
 def vectorise_chars(data, delex=False):
     """
     Output is data as before but additionally:
@@ -155,6 +187,40 @@ def vectorise_chars(data, delex=False):
     return data
 
 
+def vectorise_chars_reverse(data, delex=False):
+    """
+    Output is data as before but additionally:
+    "char_index" : char -> num index
+    "encoder_input": matrix containing all of the encoder inputs. dimensions: number of texts * max length * number
+    of tokens. One hot encoded
+    "decoder_input" : same but for output sequences
+    "decoder_target" : same but one step along
+    """
+    chars = data['char_set']
+    mr_list = data['mr_input']
+    ref_list = data['ref_sentences']
+    num_chars = data['num_chars']
+    max_mr_length = data['max_mr_length']
+    max_ref_length = data['max_ref_length']
+
+    char_index = dict([(c, i) for i, c in enumerate(chars)])
+    encoder_input = np.zeros((len(mr_list), max_ref_length, num_chars), dtype='float32')
+    decoder_input = np.zeros((len(mr_list), max_mr_length, num_chars), dtype='float32')
+    decoder_target = np.zeros((len(mr_list), max_mr_length, num_chars), dtype='float32')
+
+    for i, (mr, ref) in enumerate(zip(mr_list, ref_list)):
+        for j, c in enumerate(ref):
+            encoder_input[i, j, char_index[c]] = 1.0
+        for j, c in enumerate(mr):
+            decoder_input[i, j, char_index[c]] = 1.0
+            if j > 0:
+                decoder_target[i, j - 1, char_index[c]] = 1.0
+
+    data['char_index'] = char_index
+    data['encoder_input'] = encoder_input
+    return data
+
+
 def load_models(model_name):
     model = load_model(model_name + '.h5')
     encoder = load_model(model_name + '_encoder.h5')
@@ -167,7 +233,7 @@ def create_reverse_indices(data):
     return data
 
 
-def decode_beam_search(input, data, encoder, decoder, alpha=-1.0, beam_width=10, penalty_dict=None):
+def decode_beam_search(input, data, encoder, decoder, mr="", reverse_models=None, alpha=-1.0, beam_width=10, beta=-1.0):
     states_value = encoder.predict(input)
     target = np.zeros((1, 1, data['num_chars']))
     target[0, 0, data['char_index']['\t']] = 1.
@@ -184,7 +250,7 @@ def decode_beam_search(input, data, encoder, decoder, alpha=-1.0, beam_width=10,
         target = np.zeros((1, 1, data['num_chars']))
         target[0, 0, sampled_token_indices[i]] = 1.
         states_value = [h, c, states_value[2]]
-        top_paths.append([target, prob, decoded, False, states_value])
+        top_paths.append([target, prob, decoded, False, states_value, softmax(attention, axis=2)])
     top_paths.sort(key=lambda x: x[1], reverse=True)
     stop_condition = False
 
@@ -209,43 +275,102 @@ def decode_beam_search(input, data, encoder, decoder, alpha=-1.0, beam_width=10,
                 target[0, 0, sampled_token_indices[i]] = 1.
                 states_value = [h, c, path[4][2]]
                 sent_stop = False
+                new_att = np.concatenate((path[5], softmax(attention, axis=2)), axis=1)
                 if sampled_chars[i] == '\n' or len(decoded) > data['max_ref_length']:
                     sent_stop = True
-                all_paths.append([target, prob, decoded, sent_stop, states_value])
+                all_paths.append([target, prob, decoded, sent_stop, states_value, new_att])
         all_paths.sort(key=lambda x: x[1], reverse=True)
         top_paths = all_paths[:beam_width]
         stop_condition = True
         for path in top_paths:
             if path[3] is False:
                 stop_condition = False
-
     for path in top_paths:
         if alpha == -1.0:
             lp = len(path[2])
         else:
             lp = (math.pow(5 + len(path[2]), alpha) / math.pow(5 + 1, alpha))
-        path[1] = path[1] / lp
+        if beta == -1.0:
+            cp = 0
+        else:
+            attention = path[5]
+            attention = attention[0]
+            n = attention.shape[0]
+            m = attention.shape[1]
+            j_sum = 0
+            i_sum = 0
+            for i in range(n):
+                j_sum = 0
+                for j in range(m):
+                    j_sum += attention[i][j]
+                i_sum += math.log(min(j_sum , 1.0))
+            cp = beta * i_sum
+        if reverse_models == None:
+            rev_pen = 0
+        else:
+            rev_mr = decode_reverse(reverse_models, path[2], data)
+            rev_pen = diff_mrs(mr, rev_mr)
+
+        #path[1] = path[1] / lp + cp
+        path[1] = rev_pen
     top_paths.sort(key=lambda x: x[1], reverse=True)
     return [p[2] for p in top_paths]
 
 
-def test_model_chars(model_name, num_examples=200, delex_slots=()):
-    # https://stackoverflow.com/questions/53867351/how-to-visualize-attention-weights
+def get_mr_dict(mr):
+    mr_split = mr.split(", ")
+    mr_dict = {}
+    for slot in mr_split:
+        name = slot[:slot.find('[')]
+        mr_dict[name] = True
+    return mr_dict
+
+
+def diff_mrs(mr1, mr2):
+    mr1_dict = get_mr_dict(mr1)
+    mr2_dict = get_mr_dict(mr2)
+    diff = 0
+    for slot in mr1_dict.keys():
+        if slot not in mr2_dict:
+            diff += 1
+    for slot in mr2_dict.keys():
+        if slot not in mr1_dict:
+            diff += 1
+    return diff
+
+
+def decode_reverse(models, sentence, old_data):
+    sentence = '\t' + sentence + '\n'
+    input = np.zeros((1, old_data['max_ref_length'], old_data['num_chars']), dtype='float32')
+    for i, c in enumerate(sentence):
+        input[0, i, old_data['char_index'][c]] = 1.0
+    new_data = {'num_chars': old_data['num_chars'], 'char_index': old_data['char_index'],
+                'reverse_char_index': old_data['reverse_char_index'], 'max_ref_length': old_data['max_mr_length']}
+    decoded = decode_beam_search(input, new_data, models[0], models[1], beam_width=1)[0]
+    return decoded
+
+
+def test_model_chars(model_name, num_examples=200, delex_slots=(), alpha = -1.0, beta=-1.0, rev_pen=False):
+    # if reverse do both
     data = load_data_chars("devset.csv", delex_slots=delex_slots, delex_both=False)
     data = vectorise_chars(data)
     data = create_reverse_indices(data)
     model, encoder, decoder = load_models(model_name)
     decoder = Model(inputs=decoder.input, outputs=[decoder.output, decoder.get_layer('attention').output])
+    rev_model, rev_encoder, rev_decoder = load_models("reverse_testreverse")
+    rev_decoder = Model(inputs=rev_decoder.input, outputs=[rev_decoder.output, rev_decoder.get_layer('attention').output])
     bleus = []
     max_bleus = []
     meteors = []
-
+    print(decoder.summary())
     for seq_index in range(num_examples):
         print("Test example: " + str(seq_index) + "/" + str(len(data["encoder_input"])))
 
         input = data["encoder_input"][seq_index: seq_index + 1]
+        print(input.shape)
         correct = data['ref_sentences'][seq_index].strip("\t\n")
-        paths = decode_beam_search(input, data, encoder, decoder)
+        paths = decode_beam_search(input, data, encoder, decoder, mr=data['mr_input'][seq_index],
+                                   reverse_models=[rev_encoder, rev_decoder], beam_width=10, alpha=alpha, beta=beta)
 
         print('-')
         print(paths)
@@ -276,5 +401,4 @@ def test_model_chars(model_name, num_examples=200, delex_slots=()):
     print(sum(max_bleus) / len(max_bleus))
     print("Average METEOR: ", sum(meteors) / len(meteors))
 
-
-test_model_chars("delex_test", delex_slots=("name", "priceRange", "near", "area"))
+test_model_chars("basic2", beta=0.8)
--- L101/model_training.py
+++ L101/model_training.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import tensorflow as tf
 from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import Input, LSTM, Dense, Attention, CuDNNLSTM, Embedding
+from tensorflow.python.keras.layers import Input, LSTM, Dense, Attention, CuDNNLSTM, Embedding, Concatenate
 from tensorflow.python.keras.callbacks import TensorBoard
 
 from nltk import word_tokenize
@@ -72,6 +72,36 @@ def load_data_chars(filename="trainset.csv", delex_slots=(), delex_both=True):
     mr_list = file['mr'].to_list()
     ref_list = file['ref'].to_list()
     ref_list = [start + ref + end for ref in ref_list]
+    if len(delex_slots) > 0:
+        mr_list, ref_list = delexicalise_both(mr_list, ref_list, delex_slots)
+    else:
+        mr_list = delexicalise_mrs(mr_list, delex_slots)
+    assert(len(mr_list) == len(ref_list))
+    char_set = pickle.load(open("char_set.obj", "rb"))
+    char_set = sorted(list(char_set))
+    num_chars = len(char_set)
+    max_mr_length = max([len(mr) for mr in mr_list])
+    max_ref_length = max([len(ref) for ref in ref_list])
+
+    data_details = {'mr_input': mr_list, 'ref_sentences': ref_list, 'char_set': char_set, 'num_chars': num_chars,
+                    'max_mr_length': max_mr_length, 'max_ref_length': max_ref_length}
+    return data_details
+
+
+def load_data_chars_reverse(filename="trainset.csv", delex_slots=(), delex_both=True):
+    """
+    Output is dictionary containing:
+    "mr_input" : List of all MRs, "ref_list": List of all reference sentences
+    "char_set" : List of all characters in MRs and references
+    "num_chars" : number of characters that appear
+    "max_mr_length" : max length of MR seqs, "max_ref_length: max length of reference seqs
+    """
+    start = '\t'
+    end = '\n'
+    file = pd.read_csv(filename)
+    mr_list = file['mr'].to_list()
+    ref_list = file['ref'].to_list()
+    mr_list = [start + mr + end for mr in mr_list]
     if len(delex_slots) > 0:
         mr_list, ref_list = delexicalise_both(mr_list, ref_list, delex_slots)
     else:
@@ -162,6 +192,42 @@ def vectorise_chars(data, delex=False):
     return data
 
 
+def vectorise_chars_reverse(data, delex=False):
+    """
+    Output is data as before but additionally:
+    "char_index" : char -> num index
+    "encoder_input": matrix containing all of the encoder inputs. dimensions: number of texts * max length * number
+    of tokens. One hot encoded
+    "decoder_input" : same but for output sequences
+    "decoder_target" : same but one step along
+    """
+    chars = data['char_set']
+    mr_list = data['mr_input']
+    ref_list = data['ref_sentences']
+    num_chars = data['num_chars']
+    max_mr_length = data['max_mr_length']
+    max_ref_length = data['max_ref_length']
+
+    char_index = dict([(c, i) for i, c in enumerate(chars)])
+    encoder_input = np.zeros((len(mr_list), max_ref_length, num_chars), dtype='float32')
+    decoder_input = np.zeros((len(mr_list), max_mr_length, num_chars), dtype='float32')
+    decoder_target = np.zeros((len(mr_list), max_mr_length, num_chars), dtype='float32')
+
+    for i, (mr, ref) in enumerate(zip(mr_list, ref_list)):
+        for j, c in enumerate(ref):
+            encoder_input[i, j, char_index[c]] = 1.0
+        for j, c in enumerate(mr):
+            decoder_input[i, j, char_index[c]] = 1.0
+            if j > 0:
+                decoder_target[i, j - 1, char_index[c]] = 1.0
+
+    data['char_index'] = char_index
+    data['encoder_input'] = encoder_input
+    data['decoder_input'] = decoder_input
+    data['decoder_target'] = decoder_target
+    return data
+
+
 def vectorise_words(data, delex=False):
     """
     Output is data as before but additionally:
@@ -227,9 +293,11 @@ def build_model_chars(num_chars, lstm_units=256, layers=1, optimiser='adam'):
     attention = Attention(name='attention')
     atten_output = attention([decoder_outputs, encoder_outputs])
     atten_output.set_shape([None, None, lstm_units])
+    concat = Concatenate()
+    concat_output = concat([decoder_outputs, atten_output])
     decoder_dense = Dense(num_chars, input_shape=[None, None, lstm_units], activation='softmax',
                           name='softmax_output')
-    decoder_output = decoder_dense(atten_output)
+    decoder_output = decoder_dense(concat_output)
 
     model = Model([encoder_input, decoder_input], decoder_output)
     model.compile(optimizer=optimiser, loss='categorical_crossentropy')
@@ -244,8 +312,9 @@ def build_model_chars(num_chars, lstm_units=256, layers=1, optimiser='adam'):
     decoder_outputs, state_h, state_c = decoder_lstm(decoder_input, initial_state=decoder_states_inputs)
     #dec_attention = Attention(name='attention')([decoder_outputs, decoder_state_input_enc])
     dec_attention = attention([decoder_outputs, decoder_state_input_enc])
+    concat_output = concat([decoder_outputs, dec_attention])
     decoder_states = [state_h, state_c]
-    decoder_outputs = decoder_dense(dec_attention)
+    decoder_outputs = decoder_dense(concat_output)
     dec_state_inputs = [decoder_state_input_h, decoder_state_input_c, decoder_state_input_enc]
     decoder_model = Model([decoder_input] + dec_state_inputs, [decoder_outputs] + decoder_states)
 
@@ -320,7 +389,19 @@ def train_model_words(model_name, delex_slots=(), lstm_units=256, batch_size=64,
               batch_size=batch_size, epochs=epochs, validation_split=0.2)
 
 
+def train_reverse_model_chars(model_name, delex_slots=(), lstm_units=256, batch_size=64, epochs=25, layers=1):
+    training_data = load_data_chars_reverse(delex_slots=delex_slots)
+    training_data = vectorise_chars_reverse(training_data)
+    model, encoder_model, decoder_model = build_model_chars(training_data['num_chars'], lstm_units, layers=layers)
+    model.fit(x=[training_data["encoder_input"], training_data["decoder_input"]], y=training_data["decoder_target"],
+              batch_size=batch_size, epochs=epochs, validation_split=0.2)
+    model.save(model_name + 'reverse.h5')
+    encoder_model.save(model_name + 'reverse_encoder.h5')
+    decoder_model.save(model_name + 'reverse_decoder.h5')
+
+
 #train_model_words("test")
-#train_model_chars("basic1", epochs=50)
-train_model_chars("delex_test", delex_slots=("name", "priceRange", "near", "area"), epochs=50)
+train_model_chars("basic2", epochs=25)
+#train_model_chars("delex_test", delex_slots=("name", "priceRange", "near", "area"), epochs=50)
+#train_reverse_model_chars("reverse_test", epochs=15)
 #train_model_chars("basic512", lstm_units=512)
